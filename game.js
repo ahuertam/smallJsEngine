6ad8c87868d458476e4b336c02185c45c34b0860
Alejandro Huerta <ahuertam@yahoo.es>
2019-08-22 16:55:48 +0200

    basic movement added

--- game.js
+++ game.js
@@ -4,11 +4,21 @@ var ctx;
 const FPS= 50;
 const canvasWidth=500;
 const canvasHeight=400;
+const keyBinds ={
+  up:38,
+  down:40,
+  left:37,
+  right:39
+}
+const playerPrefs ={
+  velocity : 10
+}
 
-class character {
-  constructor(x, y) {
+class sceneObject {
+  constructor(x, y, type) {
     this.x = x;
     this.y = y;
+    this.type = type;
     this.direction=true; //true: frontal, false: backwards
     this.sense = true; // true: horizontal false: vertical
     this.endOfCanvas = false;
@@ -33,23 +43,38 @@ class character {
       ctx.fillStyle = color;
       ctx.fillRect(this.x, this.y, 20, 20);
     };
-    this.moveToCoordinate= function(axis,currentDirection,currentSense){
+    this.moveToCoordinate= function(axis,velocity,currentDirection,currentSense){
       this.direction = currentDirection;
       this.sense = currentSense;
       this.outOfBounds();
       if(!this.endOfCanvas){
         if(currentDirection) {
-          this[axis]++;
+          this[axis]+=velocity;
         } else {
-          this[axis]--;
+          this[axis]-=velocity;
         }
       }
     }
   }
 }
-const player1 = new character(10,100);  
-const obstacle = new character(40,90); 
 
+const player1 = new sceneObject(10,100,'player');  
+const obstacle = new sceneObject(40,90,'obstacle'); 
+
+document.addEventListener('keydown', function(pressedKey){
+if(pressedKey.keyCode === keyBinds.up){
+  player1.moveToCoordinate('y',playerPrefs.velocity,false, false);
+  };
+if(pressedKey.keyCode === keyBinds.down){
+    player1.moveToCoordinate('y',playerPrefs.velocity,true, false);
+    };
+if(pressedKey.keyCode === keyBinds.left){
+      player1.moveToCoordinate('x',playerPrefs.velocity,false, true);
+  };
+if(pressedKey.keyCode === keyBinds.right){
+    player1.moveToCoordinate('x',playerPrefs.velocity,true, true);
+};
+})
 const initGame=()=>{
   canvas = document.getElementById('gameCanvas'); 
   ctx = canvas.getContext('2d'); 
@@ -68,5 +93,5 @@ const main = ()=>{
   cleanCanvas();
   player1.draw("#FF0000");
   obstacle.draw("black");
-  player1.moveToCoordinate('x',true, true);
+  obstacle.moveToCoordinate('y',2,false, true);
 };
